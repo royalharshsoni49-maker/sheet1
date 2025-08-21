52b9106a3eb0a7a13c9e3e803ab2b5d2e22518b3
harsh soni <royalharshsoni49@gmail.com>
2025-08-21 10:29:16 +0530

    Create print three angle triangle .py

---
+++ print three angle triangle .py
@@ -0,0 +1,7 @@
+char=input("enter a character:")
+if char.startswith("r"):
+    print(f"right  triangle")
+elif char.startswith("o"):
+    print(f"obtusetriangle")
+elif char.startswith("a"):
+    print(f"acute triangle")
